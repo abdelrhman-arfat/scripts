dc2e1c35bf5222bcfd6070729eb636c383e00b55
boody <abdoarfat2006@gmail.com>
2025-09-28 13:42:27 +0300

    update the cd.example.sh file

--- CD.example.sh
+++ CD.example.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 REPO_URL="https://<token>@github.com/user-name/repo-name.git"
-PROJECT_DIR="repo-name" 
+PROJECT_DIR=".git" 
 BRANCH="main"
 
 git config --global user.name "user-name"
 git config --global user.email "user-email"
 
 
-if [ -d "$PROJECT_DIR/.git" ]; then
+if [ -d "$PROJECT_DIR" ]; then
   echo "🔄 Repository already exists. Pulling latest changes..."
-  cd "$PROJECT_DIR" || exit
   git pull --rebase --autostash origin $BRANCH
   echo "✅ Repository updated!"
 else
   echo "📥 Repository not found. Cloning..."
-  git clone -b $BRANCH "$REPO_URL"
-  cd "$PROJECT_DIR" || exit
+  git clone -b $BRANCH "$REPO_URL" .
   echo "✅ Repository cloned successfully!"
 fi
 
